27dac250d22b0bedabce744198433d3075e74f8c
bzap <linasalekne@gmail.com>
2023-12-24 02:39:09 -0500

    added custom popover trigger

--- frontend/src/app/interface/CustomComponents.js
+++ frontend/src/app/interface/CustomComponents.js
@@ -1,11 +1,67 @@
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import { useState, forwardRef } from "react";
+import {
+    HamburgerMenuIcon,
+    DotFilledIcon,
+    CheckIcon,
+    ChevronRightIcon,
+} from "@radix-ui/react-icons";
+
 const Button = ({ Icon, index }) => {
     return (
         <button
             type="button"
-            class={`text-black items-center transition justify-center w-full h-max flex p-3 hover:bg-gray-100 active:bg-gray-200`}
+            class={`text-black select-none outline-none items-center transition justify-center w-full h-max flex p-3 hover:bg-gray-100 active:bg-gray-200`}
         >
-            <Icon />
+            <Icon className={"stroke-2"} />
         </button>
+    );
+};
+
+const TriggerButton = forwardRef((props, forwardedRef) => {
+    return (
+        <button
+            {...props}
+            ref={forwardedRef}
+            type="button"
+            class={`text-black select-none outline-none items-center transition justify-center w-full h-max flex p-3 hover:bg-gray-100 active:bg-gray-200`}
+        >
+            <props.Icon className={"stroke-2"} />
+        </button>
+    );
+});
+
+const HorizontalDropdownMenu = ({ Icon }) => {
+    return (
+        <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+                <TriggerButton Icon={Icon} aria-label="Customise options" />
+
+                {/* <button className="IconButton" aria-label="Customise options">
+                    <HamburgerMenuIcon />
+                </button> */}
+            </DropdownMenu.Trigger>
+
+            <DropdownMenu.Portal>
+                <DropdownMenu.Content
+                    className="DropdownMenuContent"
+                    sideOffset={25}
+                    side="right"
+                >
+                    <DropdownMenu.Item className="DropdownMenuItem">
+                        Unordered List <div className="RightSlot">⌘+T</div>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="DropdownMenuItem">
+                        Ordered List <div className="RightSlot">⌘+N</div>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Item className="DropdownMenuItem">
+                        Numbered List
+                        <div className="RightSlot">⇧+⌘+N</div>
+                    </DropdownMenu.Item>
+                    <DropdownMenu.Arrow className="DropdownMenuArrow" />
+                </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+        </DropdownMenu.Root>
     );
 };
 
@@ -15,7 +71,13 @@ const ButtonGroup = ({ elements }) => {
             className={`flex mb-4 rounded-xl border-gray-200 border-[1px] flex-col overflow-hidden`}
         >
             {Object.values(elements).map((element, index) => {
-                return <Button Icon={element} index={index} length={length} />;
+                return element[1] ? (
+                    <div>
+                        <HorizontalDropdownMenu Icon={element[0]} />
+                    </div>
+                ) : (
+                    <Button Icon={element[0]} index={index} length={length} />
+                );
             })}
         </div>
     );
--- frontend/src/components/workspace/navbar/text/TextFeatureContainer.js
+++ frontend/src/components/workspace/navbar/text/TextFeatureContainer.js
@@ -1,17 +1,22 @@
 import { CustomComponents } from "@/app/interface/CustomComponents";
 import { Icons } from "@/app/interface/Icons";
 
-const TextButtonGroup = {
-    bold: Icons.Bold,
-    italic: Icons.Italic,
-    underline: Icons.Underline,
-    strikethrough: Icons.StrikeThrough,
+const TextStyleGroup = {
+    bold: [Icons.Bold],
+    italic: [Icons.Italic],
+    underline: [Icons.Underline],
+    strikethrough: [Icons.StrikeThrough],
+};
+
+const InsertionFeatureGroup = {
+    lists: [Icons.Lists, "dropdown"],
 };
 
 const TextFeatureContainer = () => {
     return (
-        <div className="flex">
-            <CustomComponents.ButtonGroup elements={TextButtonGroup} />
+        <div className="flex-col">
+            <CustomComponents.ButtonGroup elements={InsertionFeatureGroup} />
+            <CustomComponents.ButtonGroup elements={TextStyleGroup} />
         </div>
     );
 };
