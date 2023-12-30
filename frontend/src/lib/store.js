2b52e38e528957187938cb89d307364a046ae332
bzap <linasalekne@gmail.com>
2023-12-29 21:01:52 -0500

    updates to redux persist storage format related to SSR

--- frontend/src/lib/store.js
+++ frontend/src/lib/store.js
@@ -10,12 +10,29 @@ import {
     PURGE,
     REGISTER,
 } from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-import { getDefaultMiddleware } from "@reduxjs/toolkit";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+    return {
+        getItem(_key) {
+            return Promise.resolve(null);
+        },
+        setItem(_key, value) {
+            return Promise.resolve(value);
+        },
+        removeItem(_key) {
+            return Promise.resolve();
+        },
+    };
+};
+const storage =
+    typeof window !== "undefined"
+        ? createWebStorage("local")
+        : createNoopStorage();
 
 const persistConfig = {
     key: "root",
-    storage,
+    storage: storage,
     whiteList: ["markdownReducer"],
 };
 
