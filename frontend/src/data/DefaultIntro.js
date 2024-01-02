a8907fdc1ba2cc3e9ee01ada57b04c7c51253803
bzap <linasalekne@gmail.com>
2024-01-02 15:41:58 -0500

    updated default text

--- frontend/src/data/DefaultIntro.js
+++ frontend/src/data/DefaultIntro.js
@@ -1,19 +1,61 @@
-export const defaultIntro = `# Markdown syntax guide
+export const defaultIntro = `# Welcome to Mrkdwn! 🎉
 
-## Headers
+Mrkdwn is a simple, open source markdown editor[^Ref]. 
 
-# This is a Heading h1
-## This is a Heading h2
-###### This is a Heading h6
+## General Features 
+- Follows and supports GFM guidelines[^Ref2]
+- Easy insertion of commonly used elements and conversion of existing text
+  
+- Mobile friendly
+- Cached save
+- Dark mode
+- Scroll sync
+- Upload existing files and generate new ones
 
-## Emphasis
+## Examples 
+
+|Tables|Tables|Tables|
+|-------|-------|-------|
+|Content|Content|Content|
+|Content|Content|Content|
+
+[Links](google.com)
+![Images](https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png)
 
 
-## Blocks of code
+**Bolding**
+_Italics_
+~~Strikethrough~~
+==Highlighting==
+> Blockquotes
+>>Nested blockquotes
 \`\`\`
-let message = 'Hello world';
-alert(message);
-\
+const demonstration = () => {
+  console.log('Code blocks') 
+}
 \`\`\`
+\`Inline code blocks\`
 
+- Unordered
+-  List
+
+1. Ordered
+2. List
+
+- [x] Task 
+- [ ] List
+
+Footnotes[^Footnote]
+
+Dividers
+---
+
+==**All** of which _can_ be combined==. Check out the spec for more! 
+
+
+
+[^Ref]: [Repository ](https://github.com/bzap/mrkdwn)
+[^Ref2]: [GFM Reference](https://github.github.com/gfm/)
+
+[^Footnote]: I am a footnote with **styled text**.
 `;
