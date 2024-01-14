f87e11617ab3ce8d69b6a465adfe21917d97b7c9
bzap <linasalekne@gmail.com>
2024-01-13 23:59:38 -0500

    updated for mobile navbar coverage

--- frontend/src/components/WorkspaceContainer.js
+++ frontend/src/components/WorkspaceContainer.js
@@ -73,7 +73,7 @@ const WorkspaceContainer = () => {
             <div className="flex relative z-10 md-min-h-12 lg:w-24 lg:h-[96%] lg:max-h-screen lg:max-w-24 lg:min-h-screen lg:p-4 ">
                 <NavBar editorRef={editorRef} />
             </div>
-            <div className="flex relative base:z-0 lg:z-10 w-full base:w-full lg:max-w-[96%]  base:h-[calc(100vh_-_1rem)] base:pt-[3rem] lg:mt-0 lg:h-[96%]  lg:max-h-screen relative lg:min-h-screen lg:p-4 gap-3">
+            <div className="flex relative base:z-0 lg:z-10 w-full base:w-full lg:max-w-[96%]  base:h-[calc(100svh_-_1rem)] base:pt-[3rem] lg:mt-0 lg:h-[96%]  lg:max-h-screen relative lg:min-h-screen lg:p-4 gap-3">
                 <Editor editorRef={editorRef} scrollRef={firstDivRef} />
                 <Viewer scrollRef={secondDivRef} editorRef={editorRef} />
             </div>
