3555635d62e7ea126f0e67a9d9b6ac81e70eba99
bzap <linasalekne@gmail.com>
2024-01-14 00:08:55 -0500

    fixed text stat text color

--- frontend/src/components/workspace/viewer/StatPanel.js
+++ frontend/src/components/workspace/viewer/StatPanel.js
@@ -7,7 +7,7 @@ const StatPanel = () => {
     const lines = markdownText.match(/$/gm);
     return (
         <div
-            className={`w-full flex items-end px-2 py-1 bg-stone-100 dark:bg-zinc-600 gap-2 space-between border-[1px] rounded-[0.4rem] dark:text-[#CECFD0] rounded-br-lg border-stone-200 dark:border-zinc-700 text-xs font-medium`}
+            className={`w-full flex items-end px-2 py-1 bg-stone-100 dark:bg-zinc-600 text-black gap-2 space-between border-[1px] rounded-[0.4rem] dark:text-[#CECFD0] rounded-br-lg border-stone-200 dark:border-zinc-700 text-xs font-medium`}
         >
             <div>
                 {`
